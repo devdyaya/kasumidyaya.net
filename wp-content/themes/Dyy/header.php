180d32fc02228aee0aca88e6be7cd0ad43cf5e4a
devdyaya <sho.niida@shelfy.jp>
2020-04-30 01:50:51 +0900

    スタイル調整

--- wp-content/themes/Dyy/header.php
+++ wp-content/themes/Dyy/header.php
@@ -10,14 +10,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- タイトル（プラグイン出力する場合は削除） -->
 	<title><?php wp_title(); ?></title>
 
-	<!-- メタ情報（プラグイン出力する場合は削除） -->
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="author" content="">
-	<!-- OGP情報（プラグイン出力する場合は削除） -->
+
 	<meta property="og:title" content="">
 	<meta property="og:type" content="">
 	<meta property="og:url" content="">
@@ -26,11 +24,8 @@
 	<meta property="og:description" content="" />
 	<meta property="fb:app_id" content="">
 
-	<!-- ファビコン -->
 	<link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.ico">
 	<link rel="apple-touch-icon-precomposed" href="/favicon-152.png">
-
-	<!-- スタイルシート -->
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
@@ -39,32 +34,3 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-
-<div id="container">
-
-	<!-- ページヘッダ -->
-	<header id="site-header">
-		<!-- ロゴ -->
-		<?php if ( is_front_page() && is_home() ) : ?>
-		<h1 id="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<?php else : ?>
-		<p id="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></p>
-		<?php endif; ?>
-		<!-- /ロゴ -->
-
-		<!-- 検索フォーム -->
-		<?php get_search_form(); ?>
-		<!-- /検索フォーム -->
-
-		<!-- グローバルナビ -->
-		<nav>
-			<ul class="menu">
-				<li><a href="<?php echo home_url('/'); ?>">HOME</a></li>
-			</ul>
-		</nav>
-		<!-- /グローバルナビ -->
-	</header>
-	<!-- /ページヘッダ -->
-
-	<!-- main -->
-	<main id="main">
---
+++ wp-content/themes/Dyy/page-top.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: TOP PAGE
+*/
+?> 
+
+<?php get_header(); ?>
+
+
+<div class="l-container">
+	<div class="p-index">
+		<header>
+			<h1>kasumidyaya.net</h1>
+			<span>Designer</span>
+		</header>
+		<nav>
+			<ul>
+				<li>TWITTER</li>
+				<li>BLOG</li>
+			</ul>
+		</nav>
+	</div>
+</div>
