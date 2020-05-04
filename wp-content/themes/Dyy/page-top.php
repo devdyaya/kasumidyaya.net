8a579132b8b66ede88449c0a9c32730139346c64
devdyaya <sho.niida@shelfy.jp>
2020-05-04 17:16:48 +0900

    トップ出来た

--- wp-content/themes/Dyy/page-top.php
+++ wp-content/themes/Dyy/page-top.php
@@ -10,13 +10,13 @@ Template Name: TOP PAGE
 <div class="l-container">
 	<div class="p-index">
 		<header>
-			<h1>kasumidyaya.net</h1>
+			<h1>kasumidyaya</h1>
 			<span>Designer</span>
 		</header>
 		<nav>
 			<ul>
-				<li>TWITTER</li>
-				<li>BLOG</li>
+				<li><a href="https://twitter.com/kasumidyaya">Twitter</a></li>
+				<li><a href="https://">Blog</a></li>
 			</ul>
 		</nav>
 	</div>
