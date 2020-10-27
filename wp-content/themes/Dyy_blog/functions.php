23b7df80cb057ccd643868c2953798869cdbf95c
devdyaya <sho.niida@shelfy.jp>
2020-10-28 02:23:37 +0900

    画像の余白調整

--- wp-content/themes/Dyy_blog/functions.php
+++ wp-content/themes/Dyy_blog/functions.php
@@ -45,6 +45,34 @@ add_theme_support( 'post-formats', array(
 	'chat',		//チャット
 ) );
 
+// figureの不要な属性を削除
+add_shortcode('caption', 'custom_caption_shortcode');
+function custom_caption_shortcode($attr, $content = null) {
+    if ( ! isset( $attr['caption'] ) ) {
+        if ( preg_match( '#((?:<a [^>]+>s*)?<img [^>]+>(?:s*</a>)?)(.*)#is', $content, $matches ) ) {
+            $content = $matches[1];
+            $attr['caption'] = trim( $matches[2] );
+        }
+    }
+
+    $output = apply_filters('img_caption_shortcode', '', $attr, $content);
+    if ( $output != '' )
+        return $output;
+
+    extract(shortcode_atts(array(
+        'id'    => '',
+        'align' => 'alignnone',
+        'width' => '',
+        'caption' => ''
+    ), $attr, 'caption'));
+
+    if ( 1 > (int) $width || empty($caption) )
+        return $content;
+
+    if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+
+    return '<figure ' . $id . 'class="wp-caption ' . esc_attr($align) . '">' . do_shortcode( $content ) . '<figcaption class="wp-caption-text">' . $caption . '</figcaption></figure>';
+}
 // 画像のwidth,heightを削除
 add_filter( 'the_content', function( $html ){
     $html = preg_replace( '/(width|height)="\d*"\s/', '', $html );
